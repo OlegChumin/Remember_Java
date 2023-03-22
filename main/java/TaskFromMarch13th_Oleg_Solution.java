0840a3d8318b1476bb1aca1d59708270a102e2ec
Oleg Chumin <tschumin.oleg@gmail.com>
2023-03-22 12:02:17 +0300

    Add new files

--- main/java/TaskFromMarch13th_Oleg_Solution.java
+++ main/java/TaskFromMarch13th_Oleg_Solution.java
@@ -4,10 +4,10 @@ import java.io.InputStreamReader;
 import java.util.Arrays;
 import java.util.Scanner;
 
-public class TaskFromMarch13th {
+public class TaskFromMarch13th_Oleg_Solution {
     /**
      * Задание, написать программу на Java или Kotlin и транслировать ее в версию C#:
-     * 1. Считать данные с клавиатуры: Первое число - количество вводимых целых чисел (длина массива).
+     * 1. Считать данные с клавиатуры: Первое число - количество вводимых целых чисел.
      * Второе число - набор данных, массив любых целых чисел int[] array =  new array[первое число].
      * 2. Создать новый массив из исходного с сортировкой чисел. Вывести его на экран.
      * 3. Найти максимальны и минимальный элементы массива и вывести их на экран.
@@ -19,8 +19,8 @@ public class TaskFromMarch13th {
      * Требования: оформить программу в виде методов/функций.
      * Вывод на экран также оформить как отдельный метод/функцию.
      */
-    private final static Scanner scanner = new Scanner(System.in);
-    private final static BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+    private static Scanner scanner = new Scanner(System.in);
+    private static BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
 
     public static void main(String[] args) {
         // 1 способ чтения числа из строки
@@ -32,6 +32,7 @@ public class TaskFromMarch13th {
         System.out.println("min element of arrayOfInt used method getMinIntArrayElement :" + getMinIntArrayElement(arrayOfInt));
         System.out.println("max element of arrayOfInt used method  getMaxIntArrayElement" + getMaxIntArrayElement(arrayOfInt));
         getAllEvenNumbersFromIntArray(arrayOfInt);
+
 
         // 2 способ чтения числа из строки - потокобезопасный
 //        System.out.println("getIntNumberByBufferedReader  - array size: " + getIntNumberByBufferedReader());
@@ -106,21 +107,41 @@ public class TaskFromMarch13th {
         System.out.println();
     }
 
+    private static void addTwoToArrayAndPrint(int[] arrayToProcess) {
+        for (int element: arrayToProcess) {
+            element += 2;
+        }
+        System.out.println(Arrays.toString(arrayToProcess));
+    }
 
-    private static void getMaxAndMiElement(int[] array) {
+    private static void getMaxAndMiElement(int[] arrayToSort) {
         // your code here using Arrays.sort
-        // Put your code here
+        Arrays.sort(arrayToSort); // double sort in case given arrayToSort is not sorted
+        System.out.println("min element: " + "here should be min result value" + arrayToSort[0]);
+        System.out.println("max element: " + "here should be max result value" + arrayToSort[arrayToSort.length - 1]);
     }
 
     private static int[] reverseArray(int[] array) {
         // your code here using to reverse all array elements
-        int[] reversedArray = {};
-        //....
+        int[] reversedArray = new int[array.length]; // создаем новый массив элементы которого
+        // будут в обратном порядке от исходного
+        for (int i = 0; i < array.length / 2; i++) { // итерируемся до середины массива (если итерироваться по всему
+            // массиву тоисходное состояние будет возвращено
+            int temp;
+            temp = array[i]; // записываем значение из ящеек с начала массива array во временную переменную
+            reversedArray[i] = array[array.length - i - 1]; //записываем значения с конца массива array в начало reversedArray
+            reversedArray[array.length - i - 1] = temp; // записываем значение из временной переменной в концец массива reversedArray
+        }
         return reversedArray;
     }
 
     private static void getAllNumbersDividableOnThree(int[] arrayOfInt) {
-        // Put your code here
-        //...
+        System.out.println("Getting all numbers % 3 == 0 from given array...");
+        for (int i = 0; i < arrayOfInt.length; i++) {
+            if (arrayOfInt[i] % 3 == 0) {
+                System.out.print(arrayOfInt[i] + " ");
+            }
+        }
+        System.out.println();
     }
 }
